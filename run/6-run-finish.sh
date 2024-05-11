f399c393769c84f81ffa007243058da1dea9c0ed
thilina98 <thilina.avishka@gmail.com>
2024-05-11 08:43:32 +0000

    Working API

--- run/6-run-finish.sh
+++ run/6-run-finish.sh
@@ -7,11 +7,10 @@
 rm -R ../../forkgan/ForkGAN-pytorch/datasets/demo
 # rm -R ../../forkgan/ForkGAN-pytorch/results
 rm -R ../../forkgan/pre-recon/images
-# rm -R ../arkit_data/scans/$1/
-# Only deletes the directory if an argument is provided
+
+# if no arguments are provided, exits the script
 if [ "$#" -ne 1 ]; then
-    echo "ERROR: You need to pass the name of the directory you want to delete in simplerecon/arkit-data/scans/"
+    echo "ERROR: You need to pass the name of the directory, if you want to delete a directory in simplerecon/arkit-data/scans/"
     exit 1
-else:
-    rm -R ../arkit_data/scans/$1/
 fi
+rm -R ../arkit_data/scans/$1/
